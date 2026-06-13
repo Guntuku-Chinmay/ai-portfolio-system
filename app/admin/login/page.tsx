f4320bbfc59d21c0b38a4f4d99c3918be6819d80
Guntuku-Chinmay <chinmayguntuku@gmail.com>
2026-06-13 09:15:15 +0530

    feat: add admin authentication and dashboard protection

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    if (username === "admin" && password === "admin123") {
+      localStorage.setItem("admin-auth", "true");
+
+      router.push("/admin");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-md bg-white/5 border border-white/10 p-8 rounded-3xl">
+        <h1 className="text-3xl font-bold mb-6">Admin Login</h1>
+
+        <div className="grid gap-4">
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="p-3 rounded-xl bg-black border border-white/10"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-3 rounded-xl bg-black border border-white/10"
+          />
+
+          <button onClick={login} className="bg-blue-500 py-3 rounded-xl">
+            Login
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
 import ProjectForm from "../components/admin/ProjectForm";
 import ProjectsList from "../components/admin/ProjectsList";
 import MessagesList from "../components/admin/MessagesList";
 
 export default function AdminPage() {
+  const router = useRouter();
+
+  const [authorized, setAuthorized] = useState(false);
+
+  useEffect(() => {
+    const auth = localStorage.getItem("admin-auth");
+
+    if (auth === "true") {
+      setAuthorized(true);
+    } else {
+      router.push("/admin/login");
+    }
+  }, [router]);
+
+  if (!authorized) {
+    return null;
+  }
+
   return (
     <main className="min-h-screen p-10">
-      <h1 className="text-5xl font-bold mb-10">Portfolio Admin Dashboard</h1>
+      <div className="flex justify-between items-center mb-10">
+        <h1 className="text-5xl font-bold">Portfolio Admin Dashboard</h1>
+
+        <button
+          onClick={() => {
+            localStorage.removeItem("admin-auth");
+            router.push("/admin/login");
+          }}
+          className="bg-red-500 px-5 py-2 rounded-xl"
+        >
+          Logout
+        </button>
+      </div>
 
       <ProjectForm />
 
